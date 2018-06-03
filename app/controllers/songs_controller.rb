1744a27a60d18ed87572ecba82bf4855c41c827d
Brian Kelly <polymonic@gmail.com>
2018-06-02 21:01:28 -0400

    Songs can now have a key, capo, tempo and year

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -53,6 +53,6 @@ class SongsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def song_params
-      params.require(:song).permit(:title, :body, :spotify_uri)
+      params.require(:song).permit(:title, :body, :spotify_uri, :capo, :key, :tempo, :year)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_02_194146) do
+ActiveRecord::Schema.define(version: 2018_06_02_222721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2018_06_02_194146) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "spotify_uri"
+    t.integer "capo"
+    t.string "key"
+    t.integer "tempo"
+    t.integer "year"
   end
 
 end
